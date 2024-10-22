a6246a87ff033d7564052b1ce844487e2e601871
ewurasi nyarkoa <anewurasi@gmail.com>
2024-10-22 10:36:26 -0700

    set up the fetch advert

---
+++ src/components/ApiGet.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ApiGet = ({ title, description, category, media, price }) => {
+  return (
+    <div className="bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 shadow-md p-4 text-center">
+      <img src={`${media}`} alt={title} className="mb-10" />
+      <p className="text-gray-500 lowercase text-sm">{category}</p>
+      <h2 className="text-lg font-semibold">{title}</h2>
+      <p className="text-gray-600 dark:text-gray-400 mt-2 mb-4">{description}</p>
+      <p className="text-xl font-bold text-primary">${price.toFixed(2)}</p>
+    </div>
+  );
+};
+
+export default ApiGet;
